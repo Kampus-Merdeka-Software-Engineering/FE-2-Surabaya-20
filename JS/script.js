15c4e92c6fff354b9af56e6f069a7fb44592a047
FelixWahyu <felixwahyusejati89@gmail.com>
2023-12-05 20:37:33 +0700

    change page and add fecth data
    merubah beberapa tampilan dan menambah fetch data discript.js

--- JS/script.js
+++ JS/script.js
@@ -41,7 +41,49 @@ for (let i = 0; i < btns.length; i++) {
   });
 }
 
-// order menu
+// get data menus from json
+const API_BASE_URL = "http://localhost:3000";
+
+async function fetchMenus() {
+  const menuContainer = document.getElementById("menu-container");
+  try {
+    const respon = await fetch(`${API_BASE_URL}/menus`);
+    const menus = await respon.json();
+
+    const menuContent = menus.map((item) => {
+      return `
+      <div class="box">
+      <div class="icons">
+        <a href="#" class="fas fa-shopping-cart"></a>
+        <a href="#" class="fas fa-heart"></a>
+        <a href="#" class="fas fa-eye"></a>
+      </div>
+      <div class="image">
+        <img src="assets/chocolate_Drink.jpg" alt="GambarBiryani" />
+      </div>
+      <div class="content">
+        <h3>${item.name}</h3>
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+        </div>
+        <div class="price">Rp ${item.price} <span>Rp ${item.originalprice}</span></div>
+      </div>
+    </div>
+      `;
+    });
+
+    menuContainer.innerHTML = menuContent;
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchMenus();
+
+// order menu in cart
 const amount = document.getElementById("counts");
 
 function addList1() {
