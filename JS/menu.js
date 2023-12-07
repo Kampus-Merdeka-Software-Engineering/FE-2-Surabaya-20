e35e2028455dc3a505bae3136575738e75546739
FelixWahyu <felixwahyusejati89@gmail.com>
2023-12-07 14:24:10 +0700

    update change page and add img
    mengubah tampilan dan menambahkan img serta mengaubah sedikit bagian js

--- JS/menu.js
+++ JS/menu.js
@@ -41,6 +41,37 @@ for (let i = 0; i < btns.length; i++) {
   });
 }
 
+// get all data menus
+const API_BASE_URL = "http://localhost:3000";
+
+async function getAllmenus() {
+  const menuContainer = document.getElementById("cart-container");
+  try {
+    const respon = await fetch(`${API_BASE_URL}/menus`);
+    const menus = await respon.json();
+    console.log(menus);
+
+    const menuContent = menus.map((items) => {
+      return `
+      <div class="cart-item">
+      <img src="${items.image}" alt="cartItem" />
+      <div class="content">
+        <h3 id="nama1">${items.name}</h3>
+        <span class="price">Rp </span><span class="price" id="price1">${items.price}</span>
+        <div class="input"><span>Total</span><input class="totalNumber" type="number" name="total" id="total1" /></div>
+        <button class="btn-add" onclick="addList1()">add</button>
+      </div>
+    </div>
+      `;
+    });
+
+    menuContainer.innerHTML = menuContent;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getAllmenus();
+
 // order menu
 const amount = document.getElementById("counts");
 
@@ -57,94 +88,11 @@ function addList1() {
   document.getElementById("struk-content").style.display = "block";
 }
 
-function addList2() {
-  const menuName = document.getElementById("nama2");
-  const menuPrice = document.getElementById("price2");
-  const priceTotal = parseInt(document.getElementById("total2").value);
-
-  for (let i = 0; i <= priceTotal; i++) {
-    document.getElementById("subMenu").innerHTML = menuName.innerHTML + " x" + priceTotal;
-    document.getElementById("subTotal").innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-    amount.innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-  }
-  document.getElementById("struk-content").style.display = "block";
-}
-
-function addList3() {
-  const menuName = document.getElementById("nama3");
-  const menuPrice = document.getElementById("price3");
-  const priceTotal = parseInt(document.getElementById("total3").value);
-
-  for (let i = 0; i <= priceTotal; i++) {
-    document.getElementById("subMenu").innerHTML = menuName.innerHTML + " x" + priceTotal;
-    document.getElementById("subTotal").innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-    amount.innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-  }
-  document.getElementById("struk-content").style.display = "block";
-}
-
-function addList4() {
-  const menuName = document.getElementById("nama4");
-  const menuPrice = document.getElementById("price4");
-  const priceTotal = parseInt(document.getElementById("total4").value);
-
-  for (let i = 0; i <= priceTotal; i++) {
-    document.getElementById("subMenu").innerHTML = menuName.innerHTML + " x" + priceTotal;
-    document.getElementById("subTotal").innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-    amount.innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-  }
-  document.getElementById("struk-content").style.display = "block";
-}
-
-function addList5() {
-  const menuName = document.getElementById("nama5");
-  const menuPrice = document.getElementById("price5");
-  const priceTotal = parseInt(document.getElementById("total5").value);
-
-  for (let i = 0; i <= priceTotal; i++) {
-    document.getElementById("subMenu").innerHTML = menuName.innerHTML + " x" + priceTotal;
-    document.getElementById("subTotal").innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-    amount.innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-  }
-  document.getElementById("struk-content").style.display = "block";
-}
-
-function addList6() {
-  const menuName = document.getElementById("nama6");
-  const menuPrice = document.getElementById("price6");
-  const priceTotal = parseInt(document.getElementById("total6").value);
-
-  for (let i = 0; i <= priceTotal; i++) {
-    document.getElementById("subMenu").innerHTML = menuName.innerHTML + " x" + priceTotal;
-    document.getElementById("subTotal").innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-    amount.innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-  }
-  document.getElementById("struk-content").style.display = "block";
-}
-function addList7() {
-  const menuName = document.getElementById("nama7");
-  const menuPrice = document.getElementById("price7");
-  const priceTotal = parseInt(document.getElementById("total7").value);
-
-  for (let i = 0; i <= priceTotal; i++) {
-    document.getElementById("subMenu").innerHTML = menuName.innerHTML + " x" + priceTotal;
-    document.getElementById("subTotal").innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-    amount.innerHTML = "Rp " + parseInt(menuPrice.innerHTML) * priceTotal;
-  }
-  document.getElementById("struk-content").style.display = "block";
-}
-
 function clearList() {
   document.getElementById("subMenu").innerHTML = "";
   document.getElementById("subTotal").innerHTML = "";
   amount.innerHTML = "";
   document.getElementById("total1").value = "";
-  document.getElementById("total2").value = "";
-  document.getElementById("total3").value = "";
-  document.getElementById("total4").value = "";
-  document.getElementById("total5").value = "";
-  document.getElementById("total6").value = "";
-  document.getElementById("total7").value = "";
   document.getElementById("checklist-conten").style.display = "block";
   document.getElementById("order").style.display = "none";
 }
